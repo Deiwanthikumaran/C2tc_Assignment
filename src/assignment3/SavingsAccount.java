f255f6c41e19cf44cfcea8ea73b6d81cc0ab7c93
deiwanthi <deiwanthi@deiwa.lan>
2025-10-24 20:26:08 +0530

    commit

---
+++ src/assignment3/SavingsAccount.java
@@ -0,0 +1,32 @@
+package assignment3;
+
+public class SavingsAccount extends Account {
+    private double interestRate = 0.03;
+
+    public SavingsAccount(String name, double balance) {
+        super(name, balance);
+    }
+
+    @Override
+    public void deposit(double amount) {
+        balance += amount;
+        balance += balance * interestRate;
+        System.out.println("Deposited ₹" + amount + " with interest. New Balance: ₹" + balance);
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            System.out.println("Withdrew ₹" + amount + ". New Balance: ₹" + balance);
+        } else {
+            System.out.println("Insufficient balance!");
+        }
+    }
+
+    @Override
+    public double getBalance() {
+        return balance;
+    }
+}
+
